1e01511aed83f5357540495c8ba367070cfd8f7d
soras1009 <soras1009@gmail.com>
2026-04-15 16:45:19 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-// config.js
 const CONFIG = {
-  socialCenterApi: "https://www.samchully.co.kr/api/social-center.do",
-  newsCenterApi:   "https://www.samchully.co.kr/api/news-center.do",
+  socialCenterApi: "/api/social",
+  newsCenterApi:   "/api/news",
 };
