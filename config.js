44bbb0cf986431fa4a6836d050d3d9496fc1578e
soras1009 <soras1009@gmail.com>
2026-04-15 16:34:36 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,5 @@
-// =============================================
-// config.js — 설정 파일
-// =============================================
-
+// config.js
 const CONFIG = {
-  // 소셜센터 API (유튜브, 인스타, 블로그, 사보)
-  socialCenterApi: "https://www.samchully.co.kr/api/social-center.do",
-
-  // 뉴스센터 API (보도자료/뉴스)
-  newsCenterApi: "https://www.samchully.co.kr/api/news-center.do",
+  socialCenterApi: "/api/social",
+  newsCenterApi:   "/api/news",
 };
